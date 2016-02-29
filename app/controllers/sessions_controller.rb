eff6aabbb87e8f033d74a2ee5370d7b4e0ca9139
Jan Hovancik <conta.srdr@gmail.com>
2016-02-29 15:28:43 +0100

    Allow links in flash,

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       redirect_to root_path
-      flash[:success] = "Welcome to forsythia!"
+      flash[:success] = "Welcome to forsythia! Ready to #{view_context.link_to('work', '/start', class: 'text-strong')}?"
     else
       redirect_to '/login'
       flash[:error] = "Sorry, but your email or password were wrong."
